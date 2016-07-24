da1eef674ef19beabea5a20c7ceec928f982acd4
michaelmontero <monterom334@gmail.com>
2016-07-23 20:02:33 -0400

    PRUEBA

---
+++ Karepro/Migrations/201607232315372_khgfbn,.cs
@@ -0,0 +1,20 @@
+namespace Karepro.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class khgfbn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
